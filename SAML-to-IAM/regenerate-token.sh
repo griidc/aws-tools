b68a0634725e936a920f2376e9c547841f51de30
Michael Williamson <mwilliamson@mimir.tamucc.edu>
2018-09-28 16:05:11 -0500

    Adding SAML tools

---
+++ SAML-to-IAM/regenerate-token.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Generate New Token
+/home/mwilliamson/AWS/SAML-to-IAM/samlapi_formauth_adfs3.py
+
+# Install new token
+cp /home/mwilliamson/.aws/credentials /home/users/mwilliamson/.aws/credentials
+
+scp -i /home/mwilliamson/utilityvm.pem /home/mwilliamson/.aws/credentials ec2-user@ec2-34-229-16-206.compute-1.amazonaws.com:/home/ec2-user/.aws/
