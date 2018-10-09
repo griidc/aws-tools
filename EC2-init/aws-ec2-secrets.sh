e9a1acda84f54a915b536ca4d8cd6e648392435e
Michael Scott Williamson <michael.williamson@tamucc.edu>
2018-10-09 11:16:03 -0500

    Add EC2 initialization scripts

---
+++ EC2-init/aws-ec2-secrets.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Preps VM build for Pelagos Hashing
+
+# This is now in the AWS launch template,
+# but left here for reference only.
+#yum update -y
+#yum install htop automake gcc-c++ ncurses-devel git -y
+#pip install --upgrade beautifulsoup4 lxml awscli boto boto3 ntlm-auth requests-ntlm requests bs4 sts
+#sudo reboot
+
+# This is to be run as the ec2-user, via
+# an ssh connection from an account with
+# access to the aws secrets file and agent
+# forwarding is place for authentication to
+# that account.
+
+echo "yes" | scp mwilliamson@poseidon.tamucc.edu:/home/users/mwilliamson/aws-secrets.tar.bz2 .
+tar xfvj aws-secrets.tar.bz2
+
+# Generated initial Token
+/home/ec2-user/aws-tools/SAML-to-IAM/samlapi_formauth_adfs3.py
+
+# Cron re-generation of token every 4 hrs
+(crontab -l 2>/dev/null; echo "0 0,4,8,12,16,20 * * * /home/ec2-user/aws-tools/SAML-to-IAM/samlapi_formauth_adfs3.py") | crontab -
+
+
---
+++ EC2-init/aws-ec2-tools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Preps VM build for Pelagos Hashing
+
+# Add nload tool from source
+wget https://github.com/rolandriegel/nload/archive/v0.7.4.tar.gz
+tar xfvz v0.7.4.tar.gz
+cd nload-0.7.4/
+./run_autotools
+./configure
+make
+sudo make install
+
+# Add Pelagos aws-tool repo
+echo "yes" | git clone git@github.com:griidc/aws-tools.git
