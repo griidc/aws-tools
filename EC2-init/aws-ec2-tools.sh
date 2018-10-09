c7045c3244ee220771f48b23adf078b1f914fe88
Michael Scott Williamson <michael.williamson@tamucc.edu>
2018-10-09 11:37:30 -0500

    add appropriate chdirs

--- EC2-init/aws-ec2-tools.sh
+++ EC2-init/aws-ec2-tools.sh
@@ -2,6 +2,7 @@
 # Preps VM build for Pelagos Hashing
 
 # Add nload tool from source
+cd ~
 wget https://github.com/rolandriegel/nload/archive/v0.7.4.tar.gz
 tar xfvz v0.7.4.tar.gz
 cd nload-0.7.4/
@@ -11,4 +12,5 @@ make
 sudo make install
 
 # Add Pelagos aws-tool repo
+cd ~
 echo "yes" | git clone git@github.com:griidc/aws-tools.git
