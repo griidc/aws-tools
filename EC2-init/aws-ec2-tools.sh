32060ed42dcea843fb297f505fe43712ca32401f
Michael Scott Williamson <michael.williamson@tamucc.edu>
2025-08-13 10:16:47 -0500

    Much neededed modernization and simplification of AWS toolsing.

--- EC2-init/aws-ec2-tools.sh
+++ EC2-init/aws-ec2-tools.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 # Preps VM build for Pelagos Hashing
 
-# Add nload tool from source
-cd ~
-wget https://github.com/rolandriegel/nload/archive/v0.7.4.tar.gz
-tar xfvz v0.7.4.tar.gz
-cd nload-0.7.4/
-./run_autotools
-./configure
-make
-sudo make install
-
 # Add Pelagos aws-tool repo
 cd ~
 echo "yes" | git clone https://github.com/griidc/aws-tools.git
